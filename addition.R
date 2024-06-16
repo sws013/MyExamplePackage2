a33448a79c375095a61bce3e02641ee7c2478027
sws013 <sophie.shen@students.jefferson.edu>
2024-06-16 01:49:54 +0100

    Add files via upload

---
+++ addition.R
@@ -0,0 +1,14 @@
+#' Add two numbers
+#'
+#' @param x
+#' @param y
+#'
+#' @return Sum of x and y
+#'
+#' @examples
+#' addition(6, 8)
+#'
+#' @export addition
+addition <- function(x, y){
+  return(x + y)
+}
---
+++ multiplication.R
@@ -0,0 +1,17 @@
+#' Create a function of multiplication
+
+#' @param x
+#' @param y
+#'
+#' @return Mutiply x and y
+#'
+#' @examples
+#'
+#' multiplication (6, 8)
+#'
+#' @export multiplication
+multiplication <- function(x, y) {
+  return(x * y)
+}
+
+
